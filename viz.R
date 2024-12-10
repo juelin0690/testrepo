75914865aea0d5a99590fd2afb3c89f607d0b9e0
juelin0690 <liujuelin2002@163.com>
2024-12-10 03:18:05 +0000

    Add viz

---
+++ viz.R
@@ -0,0 +1,21 @@
+# for 循环
+for (i in 1:10) {
+  print(i)
+}
+
+# if-else 判断
+if (mean(data$column_name) > 10) {
+  print("Mean is greater than 10")
+} else {
+  print("Mean is less than or equal to 10")
+}# for 循环
+for (i in 1:10) {
+  print(i)
+}
+
+# if-else 判断
+if (mean(data$column_name) > 10) {
+  print("Mean is greater than 10")
+} else {
+  print("Mean is less than or equal to 10")
+}
